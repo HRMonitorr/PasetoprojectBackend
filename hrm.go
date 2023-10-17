ea994f0ef4c3598abc9577a89575a8aa3ab733a9
rofinafiin <rofinafiisr@gmail.com>
2023-10-17 16:05:12 +0100

    RELEASE V 0.0.1

---
+++ hrm.go
@@ -0,0 +1,64 @@
+package PasetoprojectBackend
+
+import (
+	json2 "encoding/json"
+	"github.com/whatsauth/watoken"
+	"net/http"
+	"os"
+)
+
+func GetDataUserFromGCF(MongoEnv, dbname, colname string) string {
+	conn := MongoCreateConnection(MongoEnv, dbname)
+	datauser := GetAllUser(conn, colname)
+	return ReturnStringStruct(datauser)
+}
+
+func ReturnStringStruct(Data any) string {
+	json, _ := json2.Marshal(Data)
+	return string(json)
+}
+
+func GCFPasetoTokenStr(PrivateKey, MongoEnv, dbname, collectionname string, r *http.Request) string {
+	var resp Credential
+	resp.Status = false
+	mconn := MongoCreateConnection(MongoEnv, dbname)
+	var datauser User
+	err := json2.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		resp.Message = "error parsing application/json: " + err.Error()
+	} else {
+		if PasswordValidator(mconn, collectionname, datauser) {
+			resp.Status = true
+			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PrivateKey))
+			if err != nil {
+				resp.Message = "Gagal Encode Token : " + err.Error()
+			} else {
+				resp.Message = "Selamat Datang"
+				resp.Token = tokenstring
+			}
+		} else {
+			resp.Message = "Password Salah"
+		}
+	}
+
+	return ReturnStringStruct(resp)
+}
+
+func GCFPasswordHasher(r *http.Request) string {
+	resp := new(Credential)
+	userdata := new(User)
+	resp.Status = false
+	err := json2.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		resp.Message = "error parsing application/json: " + err.Error()
+	} else {
+		passwordhash, err := HashPass(userdata.Password)
+		if err != nil {
+			resp.Message = "Gagal Hash Passwordnya : " + err.Error()
+		} else {
+			resp.Message = "Berhasil Hash Password"
+			resp.Token = passwordhash
+		}
+	}
+	return ReturnStringStruct(resp)
+}
---
+++ hrm_test.go
@@ -0,0 +1,61 @@
+package PasetoprojectBackend
+
+import (
+	"fmt"
+	"github.com/whatsauth/watoken"
+	"testing"
+)
+
+func TestGenerateKeyPASETO(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	fmt.Println(privateKey)
+	fmt.Println(publicKey)
+	hasil, err := watoken.Encode("cihuy", privateKey)
+	fmt.Println(hasil, err)
+}
+
+func TestHashPass(t *testing.T) {
+	password := "cihuypass"
+
+	Hashedpass, err := HashPass(password)
+	fmt.Println("error : ", err)
+	fmt.Println("Hash : ", Hashedpass)
+}
+
+func TestHashFunc(t *testing.T) {
+	conn := MongoCreateConnection("MONGOSTRING", "HRMApp")
+	userdata := new(User)
+	userdata.Username = "cihuy"
+	userdata.Password = "cihuypass"
+
+	data := GetOneUser(conn, "user", User{
+		Username: userdata.Username,
+		Password: userdata.Password,
+	})
+	fmt.Printf("%+v", data)
+	fmt.Println(" ")
+	hashpass, _ := HashPass(userdata.Password)
+	fmt.Println("Hasil hash : ", hashpass)
+	compared := CompareHashPass(userdata.Password, data.Password)
+	fmt.Println("result : ", compared)
+}
+
+func TestTokenEncoder(t *testing.T) {
+	conn := MongoCreateConnection("MONGOSTRING", "HRMApp")
+	privateKey, publicKey := watoken.GenerateKey()
+	userdata := new(User)
+	userdata.Username = "cihuy"
+	userdata.Password = "cihuypass"
+
+	data := GetOneUser(conn, "user", User{
+		Username: userdata.Username,
+		Password: userdata.Password,
+	})
+	fmt.Println("Private Key : ", privateKey)
+	fmt.Println("Public Key : ", publicKey)
+	fmt.Printf("%+v", data)
+	fmt.Println(" ")
+
+	encode := TokenEncoder(data.Username, privateKey)
+	fmt.Printf("%+v", encode)
+}
