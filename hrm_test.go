20230f1fe6b2f430a5bb3061a11f47fe1ea1c837
rofinafiin <rofinafiisr@gmail.com>
2023-10-19 14:17:52 +0100

    Membuat paseto encoder dan decode menyesuaikan role dan username

--- hrm_test.go
+++ hrm_test.go
@@ -6,6 +6,10 @@ import (
 	"testing"
 )
 
+var privatekey = "privatekey"
+var publickeyb = "publickey"
+var encode = "encode"
+
 func TestGenerateKeyPASETO(t *testing.T) {
 	privateKey, publicKey := watoken.GenerateKey()
 	fmt.Println(privateKey)
@@ -80,4 +84,29 @@ func TestCompareUsername(t *testing.T) {
 		"token")
 	compare := CompareUsername(conn, "user", deco)
 	fmt.Println(compare)
+}
+
+func TestEncodeWithRole(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	role := "admin"
+	username := "cihuy"
+	encoder, err := EncodeWithRole(role, username, privateKey)
+
+	fmt.Println(" error :", err)
+	fmt.Println("Private :", privateKey)
+	fmt.Println("Public :", publicKey)
+	fmt.Println("encode: ", encoder)
+
+}
+
+func TestDecoder2(t *testing.T) {
+	pay, err := Decoder(publickeyb, encode)
+	user, _ := DecodeGetUser(publickeyb, encode)
+	role, _ := DecodeGetRole(publickeyb, encode)
+	use, ro := DecodeGetRoleandUser(publickeyb, encode)
+	fmt.Println("user :", user)
+	fmt.Println("role :", role)
+	fmt.Println("user and role :", use, ro)
+	fmt.Println("err : ", err)
+	fmt.Println("payload : ", pay)
 }
--- struct.go
+++ struct.go
@@ -1,5 +1,7 @@
 package PasetoprojectBackend
 
+import "time"
+
 type User struct {
 	Username string `json:"username" bson:"username"`
 	Password string `json:"password" bson:"password"`
@@ -25,4 +27,12 @@ type Response struct {
 type ResponseEncode struct {
 	Message string `json:"message,omitempty" bson:"message,omitempty"`
 	Token   string `json:"token,omitempty" bson:"token,omitempty"`
+}
+
+type Payload struct {
+	User string    `json:"user"`
+	Role string    `json:"role"`
+	Exp  time.Time `json:"exp"`
+	Iat  time.Time `json:"iat"`
+	Nbf  time.Time `json:"nbf"`
 }
